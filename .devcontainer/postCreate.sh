ac2d787874a3b4732acbfc84244c7f9fb28f822c
Docker <docker@example.com>
2026-04-23 13:52:34 +0200

    upgrade colombe dependency

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -7,4 +7,9 @@ sudo chown -R opam: _build
 
 opam init -a --shell=zsh
 
+opam remote remove --all default
+opam repository add default --all-switches --set-default https://opam.ocaml.org
+
+opam install --with-test --with-doc --deps-only -y .
+
 make deps
--- lib/letters.ml
+++ lib/letters.ml
@@ -69,13 +69,7 @@ let stream_of_string s =
 
 let str_to_colombe_address (str_address : string) : Colombe.Forward_path.t =
   match Emile.of_string str_address with
-  | Ok mailbox ->
-    (match
-       (Colombe_emile.to_forward_path mailbox
-        : (Colombe.Forward_path.t, [> `Msg of string ]) result)
-     with
-     | Ok address -> address
-     | Error _ -> raise (Invalid_email_address str_address))
+  | Ok mailbox -> Colombe_emile.to_forward_path mailbox
   | Error _ -> raise (Invalid_email_address str_address)
 ;;
 
@@ -238,14 +232,7 @@ let send =
       | Ok v -> v
       | Error (`Invalid (_, _)) -> failwith "Invalid sender address"
     in
-    let from_addr : Colombe.Reverse_path.t =
-      match
-        (Colombe_emile.to_reverse_path from_mailbox
-         : (Colombe.Reverse_path.t, [> `Msg of string ]) result)
-      with
-      | Ok v -> v
-      | Error (`Msg msg) -> failwith msg
-    in
+    let from_addr = Colombe_emile.to_reverse_path from_mailbox in
     let recipients =
       List.map
         (fun recipient ->
